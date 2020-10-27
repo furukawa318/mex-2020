feff2ce4270e36487f9cb6c9e1a4dd0f38a122d1
FurukawaMamiko <aquamarine.mamiko@gmail.com>
2020-10-27 14:58:41 +0900

    リサイズを実装した

--- js/index.js
+++ js/index.js
@@ -28,8 +28,8 @@ function init() {
   });
 
   // サイズを指定
-  const width = 960;
-  const height = 540;
+  //const width = 960;
+  //const height = 540;
 
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
@@ -37,13 +37,13 @@ function init() {
   });
 
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   // シーンを作成
   const scene = new THREE.Scene();
 
   // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, width / height);
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
   camera.position.set(0, 0, +1000);
 
   // 箱を作成
