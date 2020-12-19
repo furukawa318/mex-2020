f3217396905f9883ed6ff72f17191778a2c3ba0c
FurukawaMamiko <aquamarine.mamiko@gmail.com>
2020-12-19 13:51:33 +0900

    モデル表示１０（まだ続くよ）

--- js/index.js
+++ js/index.js
@@ -153,8 +153,10 @@ function init() {
         // 近くなる
         cameraControls.fitTo(intersects[0].object, true);
         // ARモデルが変わる
-        console.log(`assets/models/${panel.name}.glb`);
-        document.getElementById('ARchange').src = `assets/models/mamiko${i}.glb`;
+        for (let j = 0; j< 4; j++){
+          console.log(`assets/models/${panel.name}.glb`);
+          document.getElementById('ARchange').src = `assets/models/mamiko${j}.glb`;
+        }
       } 
     });
   }
